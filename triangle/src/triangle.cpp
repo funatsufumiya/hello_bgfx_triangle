2da65ee2cd6422fecd821abe9c05d58bed510fbe
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-01-01 20:02:01 +0900

    add triangle example

--- triangle/src/triangle.cpp
+++ triangle/src/triangle.cpp
@@ -3,6 +3,24 @@
 #include <big2.h>
 #include <cstdint>
 
+#include <big2/bgfx/embedded_shader.h>
+#include <generated/shaders/triangle/all.h>
+
+#include <glm/glm.hpp>
+
+struct NormalColorVertex
+{
+    glm::vec2 position;
+    uint32_t color;
+};
+
+static const bgfx::EmbeddedShader kEmbeddedShaders[] =
+    {
+        BGFX_EMBEDDED_SHADER(vs_basic),
+        BGFX_EMBEDDED_SHADER(fs_basic),
+        BGFX_EMBEDDED_SHADER_END()
+    };
+
 static void GlfwErrorCallback(int error, const char* description)
 {
     std::cerr << "GLFW Error " << error << ": " << description << std::endl;
@@ -46,6 +64,34 @@ int main(int, char**)
 
     bgfx::setDebug(BGFX_DEBUG_TEXT);
 
+    bgfx::RendererType::Enum renderer_type = bgfx::getRendererType();
+    bgfx::ProgramHandle program = bgfx::createProgram(
+            bgfx::createEmbeddedShader(kEmbeddedShaders, renderer_type, "vs_basic"),
+            bgfx::createEmbeddedShader(kEmbeddedShaders, renderer_type, "fs_basic"),
+            true
+    );
+
+    bgfx::VertexLayout color_vertex_layout;
+    color_vertex_layout.begin()
+                    .add(bgfx::Attrib::Position, 2, bgfx::AttribType::Float)
+                    .add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true)
+                    .end();
+
+    NormalColorVertex kTriangleVertices[] =
+        {
+                {{-0.5f, -0.5f}, 0x339933FF},
+                {{0.5f, -0.5f}, 0x993333FF},
+                {{0.0f, 0.5f}, 0x333399FF},
+        };
+
+    const uint16_t kTriangleIndices[] =
+            {
+                    0, 1, 2,
+            };
+
+    bgfx::VertexBufferHandle vertex_buffer = bgfx::createVertexBuffer(bgfx::makeRef(kTriangleVertices, sizeof(kTriangleVertices)), color_vertex_layout);
+    bgfx::IndexBufferHandle index_buffer = bgfx::createIndexBuffer(bgfx::makeRef(kTriangleIndices, sizeof(kTriangleIndices)));
+
     // We will use this to reference where we're drawing
     const bgfx::ViewId main_view_id = 0;
 
@@ -67,6 +113,17 @@ int main(int, char**)
 
         // Ensure the view is redrawn even if no graphic commands are called
         bgfx::touch(main_view_id);
+
+        bgfx::setState(
+            BGFX_STATE_WRITE_R
+                    | BGFX_STATE_WRITE_G
+                    | BGFX_STATE_WRITE_B
+                    | BGFX_STATE_WRITE_A
+        );
+
+        bgfx::setVertexBuffer(0, vertex_buffer);
+        bgfx::setIndexBuffer(index_buffer); // not needed if you don't do indexed draws
+        bgfx::submit(main_view_id, program);
 
         // End the frame
         bgfx::frame();
