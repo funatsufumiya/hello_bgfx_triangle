ded3093356f91b1c5f9546d01a958827bdbf9c18
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-01-04 10:40:54 +0900

    fix: update run script

--- run.sh
+++ run.sh
@@ -6,4 +6,12 @@ if [ -z "$NAME" ]; then
     NAME="triangle"
 fi
 
-./build/triangle/big2_example_${NAME}
+# if only exists, then run
+exe=./build/triangle/big2_example_${NAME}
+if [ -f "$exe" ]; then
+    echo "Running $NAME example"
+    $exe
+else
+    echo "Example $NAME does not exist"
+    exit 1
+fi
